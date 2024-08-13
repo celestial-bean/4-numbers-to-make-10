8a21f6c4579e71831acea8412458dac44330dffd
Logan <92747680+celestial-bean@users.noreply.github.com>
2024-08-13 18:21:37 -0500

    Update 10.py

--- 10.py
+++ 10.py
@@ -23,11 +23,11 @@ try:
 except ImportError:
     website=False
 
-def output(value):
+def output(value=""):
     if website:
         HTMLoutput.textContent+=value
     else:
-        print(value)
+        output(value)
 
 answers=[]
 ops = ["+", "*", "/", "-"]
@@ -35,22 +35,22 @@ def calculate(num1, num2, num3, num4, op1, op2, op3, parenthesis, end):
     result=0
     try:
         calc=num1 + ops[op1] + num2 + ops[op2] + num3 + ops[op3] +num4
-        #print(parenthesis)
+        #output(parenthesis)
         if (parenthesis != 6):
             calc=calc[:parenthesis]+"("+calc[parenthesis:3+end]+")"+calc[3+end:]
         else:
-            #print(calc)
+            #output(calc)
             pass
         if(calc[0]=="(" and calc[-1]==")"):
             calc=calc[1:-1]
         result=eval(calc)
     except ZeroDivisionError:
-        #print("Can't divide by zero")
+        #output("Can't divide by zero")
         pass
-    #print(calc)
+    #output(calc)
     if result == 10 and not calc in answers:
         answers.append(calc)
-        print(calc)
+        output(calc)
         pass
 #4+6+8+3
 def main(nums):
@@ -64,9 +64,9 @@ def main(nums):
             for op2 in range(4):
                 for op3 in range(4):
                     for parenthesis in range(0,7,2):
-                        #print(parenthesis)
+                        #output(parenthesis)
                         for end in  range(parenthesis, 7, 2):
-                            #print(parenthesis)
+                            #output(parenthesis)
                             calculate(*nums,op1,op2,op3,parenthesis,end)
         count=count+1
     
@@ -77,8 +77,8 @@ while tryAgain == "y":
     else:
         nums = [input("number " + str(i + 1) + ": ") for i in range(4)]
     main(nums)
-    print()
-    print("Total: "+str(len(answers)))
+    output()
+    output("Total: "+str(len(answers)))
     if website:
         @when("click", submit)
         def click(event):
